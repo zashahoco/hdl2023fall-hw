c00034cddebddbd11b121840252fc0d4fcdd91e8
zashahoco <133637493+zashahoco@users.noreply.github.com>
2023-10-25 06:05:23 +0000

    codespace

--- week06/ex01/bcdfadd4.v
+++ week06/ex01/bcdfadd4.v
@@ -7,5 +7,16 @@ module bcdfadd4(
 );
 
   // write your code here
-  
+  reg [4:0] _sum;
+
+  assign sum = _sum[3:0];
+  assign cout = _sum[4];
+
+  always @* begin
+    _sum = a + b + cin;
+    if (_sum > 9) begin
+      _sum = _sum - 10;
+      _sum[4] = 1'b1;
+    end
+  end
 endmodule
